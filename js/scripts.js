55a5b8a525b4bf058731793e8eaec98ba890c375
justinkwanchan <justinkwanchan@gmail.com>
2021-03-19 23:32:01 -0400

    Finish slide navigation animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
-/* Clicking on a section name in nav will scroll to that section */
+/**
+ * Clicking on a section name in nav will scroll to that section
+ */
 const sections = document.getElementsByClassName('fp-tooltip');
 
 for (let i = 0; i < sections.length; i++) {
   sections[i].addEventListener('click', () => fullpage_api.moveTo(i + 1));
 }
 
-/* Clicking on a project image will scroll to that slide */
+/**
+ * Clicking on a project image will scroll to that slide
+ */
 const projects = document.getElementsByClassName('portfolio__item');
 
 for (let i = 0; i < projects.length; i++) {
   projects[i].addEventListener('click', () => fullpage_api.moveTo(3, i + 1));
 }
 
-/* Add project titles to project slide navigation */
+/**
+ * Add project titles to project slide navigation
+ */
 const projectSlides = document.querySelectorAll(
   '.fp-slidesNav ul li a span.fp-sr-only'
 );
@@ -29,7 +35,13 @@ const titles = [
 
 projectSlides.forEach((slide, index) => (slide.textContent = titles[index]));
 
-/* Project slide change */
+/**
+ * Add class to active project ul element to active animation
+ */
+document
+  .querySelector(`.fp-slidesNav ul li:nth-child(1) a`)
+  .classList.add('project-slide');
+
 const observer = new MutationObserver(mutations => {
   mutations.forEach(mutation => {
     if (mutation.attributeName === 'class') {

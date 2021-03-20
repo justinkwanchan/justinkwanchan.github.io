60deaaf3f5e6233fc93a5b12421193b309419d65
justinkwanchan <justinkwanchan@gmail.com>
2021-03-19 21:40:41 -0400

    Fix spacing of project slide navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
-// Clicking on a section name in nav will scroll to that section
+/* Clicking on a section name in nav will scroll to that section */
 const sections = document.getElementsByClassName('fp-tooltip');
 
 for (let i = 0; i < sections.length; i++) {
   sections[i].addEventListener('click', () => fullpage_api.moveTo(i + 1));
 }
 
-// Clicking on a project image will scroll to that slide
+/* Clicking on a project image will scroll to that slide */
 const projects = document.getElementsByClassName('portfolio__item');
 
 for (let i = 0; i < projects.length; i++) {
   projects[i].addEventListener('click', () => fullpage_api.moveTo(3, i + 1));
 }
 
-// Add project titles to project slide navigation
-
+/* Add project titles to project slide navigation */
 const projectSlides = document.querySelectorAll(
   '.fp-slidesNav ul li a span.fp-sr-only'
 );
